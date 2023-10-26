8784395eae56909aea72b002298de7eed2f17e22
mmlegenkiy <93904802+mmlegenkiy@users.noreply.github.com>
2023-10-26 19:41:01 +0300

    Add old file deleting

--- myscript.sh
+++ myscript.sh
@@ -5,5 +5,6 @@ BDATE=$(date +'%d.%m.%Y_%H.%M.%S')
 FILENAME=$BDIR/$(hostname)_$BDATE
 TARGET='/home/ivanov_i'
 
+find $BDIR -mtime +2 -delete
 tar -czvf $FILENAME.tar.gz  $TARGET &> /$FILENAME.log
 
